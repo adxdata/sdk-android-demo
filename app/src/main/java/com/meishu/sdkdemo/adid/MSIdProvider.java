fc4dfe5836f68b25476819b9a37aee434c7abe74
Lei Dai <2-meishu@users.noreply.git.adxdata.com>
2020-03-23 00:59:04 +0800

    界面上可以输入广告位ID

--- app/src/main/java/com/meishu/sdkdemo/adid/MSIdProvider.java
+++ app/src/main/java/com/meishu/sdkdemo/adid/MSIdProvider.java
@@ -3,76 +3,92 @@ package com.meishu.sdkdemo.adid;
 public class MSIdProvider implements IAdIdProvider {
     @Override
     public String rewardPortrait() {
-        return "100424018";
+        //激励视频-竖版
+        return "73646b0799001991";
     }
 
     @Override
     public String rewardLandscape() {
-        return "1004468";
+        //激励视频-横版
+        return "73646b0799001991";
     }
 
     @Override
     public String feedVideo() {
+        //未调用
         return "100424126";
     }
 
     @Override
     public String feedThreeImgs() {
-            return "100424039";
+        //信息流-三图
+        return "73646b0101001991";
     }
 
     @Override
     public String feedImageVertical() {
-        return "100424017";
+        //信息流-大图 上文下图
+        return "73646b0101001991";
     }
 
     @Override
     public String feedImageHorizon() {
-        return "100424123";
+        //信息流-小图 左图右文
+        return "73646b0103001991";
     }
 
     @Override
     public String feedImageHorizonDesc() {
+        //未调用
         return "100424125";
     }
 
     @Override
     public String feedPreRender() {
-        return "100424017";
+        //信息流-模板渲染
+        return "73646b0299001991";
     }
 
     @Override
     public String video() {
+        //未调用
         return "100424122";
     }
 
     @Override
     public String paster() {
-        return "100424034";
+        //贴片
+        //return "73646b0699001991";  //素材随机
+        return "73646b0602001991";  //视频
     }
 
     @Override
     public String image() {
+        //未调用 NativeAdSelectActivity -> ImageNotInRecyclerActivity
         return "100424120";
     }
 
     @Override
     public String insertScreen() {
-        return "100424020";
+        //插屏
+        return "73646b0599001991";
     }
 
     @Override
     public String splash() {
-       return "100424024";
+        //开屏
+        return "73646b0499001991";
     }
 
     @Override
     public String banner() {
-        return "100424117";
+        //BANNER
+        return "73646b0399001991";
     }
 
     @Override
     public String videoFeed() {
+        //DRAW信息流
         return "";
     }
 
