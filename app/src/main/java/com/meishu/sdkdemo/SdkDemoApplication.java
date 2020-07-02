f9fb81ec0bbde6c806cceaa037876cb4c9488e19
liangshuo <5-liangshuo@users.noreply.git.adxdata.com>
2020-07-02 15:27:49 +0800

    提前设置 debug，防止有些初始化 log 不打印
    Signed-off-by: liangshuo <5-liangshuo@users.noreply.git.adxdata.com>

--- app/src/main/java/com/meishu/sdkdemo/SdkDemoApplication.java
+++ app/src/main/java/com/meishu/sdkdemo/SdkDemoApplication.java
@@ -14,6 +14,9 @@ public class SdkDemoApplication extends Application {
         // 设置请求广告是否使用 https，默认不使用，需要在 init 之前调用
 //        AdSdk.setHttps(true);
 
+        // 设置是否 debug 模式，debug 模式会打印内部 log，默认不打印
+        AdSdk.setDebug(true);
+
         //一定要在Application中初始化sdk，否则无法正常使用sdk
         //appId是在美数注册的appId
         //testModeEnabled表示是否开启测试模式，测试模式无法用于生产环境
@@ -21,9 +24,6 @@ public class SdkDemoApplication extends Application {
 
         // 设置下载提示类型，默认不提示 AdSdk.DOWNLOAD_MODE_DIRECTLY
         AdSdk.setDownloadMode(AdSdk.DOWNLOAD_MODE_NOTIFY);
-
-        // 设置是否 debug 模式，debug 模式会打印内部 log，默认不打印
-        AdSdk.setDebug(true);
 
         // 设置请求广告的 secure 参数，一般不需要设置，默认 SECURE_ALL
 //        AdSdk.setSecure(AdSdk.SECURE_HTTP);
