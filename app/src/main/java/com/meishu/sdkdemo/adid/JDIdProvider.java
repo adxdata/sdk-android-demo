777958098de11146df41101bff3dc054a0d7e6eb
fan <18-fanshicheng@users.noreply.git.adxdata.com>
2020-12-01 10:11:07 +0800

    添加京东

---
+++ app/src/main/java/com/meishu/sdkdemo/adid/JDIdProvider.java
@@ -0,0 +1,88 @@
+package com.meishu.sdkdemo.adid;
+
+public class JDIdProvider implements IAdIdProvider {
+    @Override
+    public String rewardPortrait() {
+        return null;
+    }
+
+    @Override
+    public String rewardLandscape() {
+        return null;
+    }
+
+    @Override
+    public String feedVideo() {
+        return null;
+    }
+
+    @Override
+    public String feedThreeImgs() {
+        return null;
+    }
+
+    @Override
+    public String feedImageVertical() {
+        return null;
+    }
+
+    @Override
+    public String feedImageHorizon() {
+        return null;
+    }
+
+    @Override
+    public String feedImageHorizonDesc() {
+        return null;
+    }
+
+    @Override
+    public String feedPreRender() {
+        return null;
+    }
+
+    @Override
+    public String video() {
+        return null;
+    }
+
+    @Override
+    public String paster() {
+        return null;
+    }
+
+    @Override
+    public String image() {
+        return null;
+    }
+
+    @Override
+    public String insertScreen() {
+        return null;
+    }
+
+    @Override
+    public String splash() {
+        return "73646b0402091191";
+    }
+
+    @Override
+    public String banner() {
+        return null;
+    }
+
+    @Override
+    public String videoFeed() {
+        return null;
+    }
+
+    @Override
+    public String fullScreenVideo() {
+        return null;
+    }
+
+    @Override
+    public String platformName() {
+        return null;
+    }
+}
